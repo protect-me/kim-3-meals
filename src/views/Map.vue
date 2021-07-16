043ed00ceb3a1d2efdea7b4165e693c1cd54ebf6
protect-me <pmfwiww@gmail.com>
2021-07-16 18:36:14 +0900

    Feat: test kakaomap

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,13 +1,45 @@
 <template>
-  <div>Map</div>
+  <div>
+    <div
+      id="map"
+      class="map"></div>
+  </div>
 </template>
 
 <script>
 export default {
+  mounted() {
+    window.kakao && window.kakao.maps
+      ? this.initMap()
+      : this.addKakaoMapScript();
+  },
+  methods: {
+    addKakaoMapScript() {
+      const script = document.createElement("script");
+      /* global kakao */
+      script.onload = () => kakao.maps.load(this.initMap);
+      script.src =
+        "http://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=486a425ae547a5bc41030e47f9828d0a"; 
+      document.head.appendChild(script);
+    },
+    initMap() {
+      var container = document.getElementById("map"); //지도를 담을 영역의 DOM 레퍼런스
+      var options = {
+        //지도를 생성할 때 필요한 기본 옵션
+        center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+        level: 3 //지도의 레벨(확대, 축소 정도)
+      };
 
-}
+      var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+      console.log(map);
+    }
+  }
+};
 </script>
 
 <style>
-
+.map {
+  width: 100%;
+  height: 50vh;
+}
 </style>
