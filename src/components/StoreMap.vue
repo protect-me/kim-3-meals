99305322554368aff8c6cf67a0ca448642ad9caf
protect-me <pmfwiww@gmail.com>
2021-07-18 22:22:31 +0900

    Feat: add eventListener for customoverlay

--- src/components/StoreMap.vue
+++ src/components/StoreMap.vue
@@ -2,7 +2,7 @@
   <div
     id="map"
     class="map">
-  </div>
+  </div>  
 </template>
 
 <script>
@@ -11,6 +11,11 @@ export default {
     stores: {
       type: Object,
       default: () => ({})
+    }
+  },
+  data() {
+    return {
+      selectedMarker: null
     }
   },
   mounted() {
@@ -28,6 +33,7 @@ export default {
       document.head.appendChild(script);
     },
     initMap() {
+      let that = this
       var container = document.getElementById("map"); //지도를 담을 영역의 DOM 레퍼런스
       var options = {
         //지도를 생성할 때 필요한 기본 옵션
@@ -43,23 +49,24 @@ export default {
 
       // 마커 이미지의 이미지 주소
       var imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png"; 
+      // var clickedImageSrc = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png'; 
+      // 마커 이미지의 이미지 크기 세팅
+      var imageSize = new kakao.maps.Size(24, 35); 
+      // 마커 이미지를 생성
+      var normalImage = new kakao.maps.MarkerImage(imageSrc, imageSize); 
+      // var clickedImage = new kakao.maps.MarkerImage(clickedImageSrc, imageSize); 
+      // set store latlng 
       for (var i = 0; i < this.stores.length; i ++) {         
-        // 마커 이미지의 이미지 크기 세팅
-        var imageSize = new kakao.maps.Size(24, 35); 
-        // 마커 이미지를 생성
-        var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize); 
-        // set store latlng 
-        const latlng = new kakao.maps.LatLng(this.stores[i].lat, this.stores[i].lng)        
         // 마커 생성
+        const latlng = new kakao.maps.LatLng(this.stores[i].lat, this.stores[i].lng)        
         var marker = new kakao.maps.Marker({ // eslint-disable-line no-unused-vars
           map: map, // 마커를 표시할 지도
           position: latlng, // 마커를 표시할 위치
           title : this.stores[i].name, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
-          image : markerImage // 마커 이미지
+          image : normalImage // 마커 이미지
         });
-
-        // 인포윈도우의 스타일을 강제로 변경할 수는 있지만, 카카오 지도의 정책에 맞게 커스텀 오버레이를 활용        
-        var content = 
+        
+        var basicInfo = 
           `<div class="customoverlay">
             <a href="https://map.kakao.com/link/map/11394059" 
               target="_blank">
@@ -70,26 +77,70 @@ export default {
           </div>`
         var customOverlay = new kakao.maps.CustomOverlay({
           clickable: true,
-          content: content,
+          content: basicInfo,
           position: latlng,
           xAnchor: 0.5,
           yAnchor: 2.3,
           zIndex: 3
         });
 
-
-        kakao.maps.event.addListener(marker, 'mouseover', makeOverListener(map, customOverlay));
+        var moreInfo = 
+          `<div class="customoverlay">
+            <a href="https://map.kakao.com/link/map/11394059" 
+              target="_blank">
+              <span class="title">
+                ${this.stores[i].name}
+                more!!
+              </span>
+            </a>
+          </div>`
+        var customOverlayMore = new kakao.maps.CustomOverlay({
+          clickable: true,
+          content: moreInfo,
+          position: latlng,
+          xAnchor: 0.5,
+          yAnchor: 2.3,
+          zIndex: 3
+        });
+        kakao.maps.event.addListener(map, 'click', function() {
+          // 지도 클릭시 선택되어ㅣ있는 customOverlay 제거
+          if (that.selectedMarker) {
+            console.log('Map Clicked!');
+            that.selectedMarker.overlay.setMap(null); // 기존 ovelay 제거
+          }
+        })
+        kakao.maps.event.addListener(marker, 'mouseover', makeOverListener(map, marker, customOverlay));
         kakao.maps.event.addListener(marker, 'mouseout', makeOutListener(map, customOverlay));
-        // 다른 infowindowMore을 열면 이전에 열린 요소가 닫혀야하는데 이러면 이벤트 위임으로 부모 요소에 적용하면 되겠다.
+        kakao.maps.event.addListener(marker, 'click', makeOverClickListener(map, marker, customOverlayMore));
       }
-      function makeOverListener(map, customOverlay){
+      function makeOverListener(map, marker, customOverlay){
         return function() {
-          customOverlay.setMap(map); // 지도에 올림
+          if (that.selectedMarker) {
+            // 선택되어 있는 마커가 있을 경우
+            if (marker.Fb !== that.selectedMarker.Fb) {
+              // 선택되어 있는 마커와 새로 선택된 마커의 상호명이 같을 경우
+              customOverlay.setMap(map); // 지도에 올림
+            }
+          } else {
+            // 선택되어 있는 마커가 없을 경우
+            customOverlay.setMap(map); // 지도에 올림
+          }
         };
       }
       function makeOutListener(map, customOverlay) {
         return function() {
           customOverlay.setMap(null); // 지도에 제거
+        };
+      }
+      function makeOverClickListener(map, marker, customOverlayMore){
+        return function() {
+          if (that.selectedMarker) {
+            that.selectedMarker.overlay.setMap(null); // 기존 ovelay 제거
+            that.selectedMarker.overlay = null;
+          }
+          that.selectedMarker = marker
+          customOverlayMore.setMap(map); // 지도에 올림
+          marker.overlay = customOverlayMore
         };
       }
     }
@@ -107,7 +158,9 @@ export default {
 }
 .customoverlay {
   position:relative;bottom:0px;border-radius:6px;border: 1px solid #ccc;border-bottom:2px solid #ddd;float:left;
-  a {display:block;text-decoration:none;color:#000;text-align:center;border-radius:6px;font-size:14px;font-weight:bold;overflow:hidden;background: $primary;background: $primary url(https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/arrow_white.png) no-repeat right 14px center;}
+  a {display:block;text-decoration:none;color:#000;text-align:center;border-radius:6px;font-size:14px;font-weight:bold;overflow:hidden;background: $primary;
+    /* background: $primary url(https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/arrow_white.png) no-repeat right 14px center; */
+    }
   .title {display:block;text-align:center;background:#fff;margin-right:35px;padding:10px 15px;font-size:14px;font-weight:bold;}
 }
 .customoverlay:nth-of-type(n) {border:0; box-shadow:0px 1px 2px #888;}
