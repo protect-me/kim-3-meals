6a9d6cadd1e22da61a93541546d5dfa84ce030fd
protect-me <pmfwiww@gmail.com>
2021-08-18 21:18:53 +0900

    Feat: edit panToPoint when marker clicked

--- src/components/StoreMap.vue
+++ src/components/StoreMap.vue
@@ -226,6 +226,8 @@ export default {
         };
       }
       function panTo(map, latlng) {
+        console.log(latlng);
+        latlng.Ma += 0.02;
         // 지도 중심을 부드럽게 이동시킵니다
         // 만약 이동할 거리가 지도 화면보다 크면 부드러운 효과 없이 이동)
         map.panTo(latlng);
